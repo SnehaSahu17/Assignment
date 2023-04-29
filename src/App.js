918bb565c6106851928430e249d09574c2788297
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-04-29 21:57:53 +0530

    ft:section two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
 
 function App() {
   return (
     <div className="bg-black w-full h-full">
      <div className="pt-14 px-8"><Navbar/></div>
      <SectionOne/>
+     <SectionTwo/>
     </div>
   );
 }
